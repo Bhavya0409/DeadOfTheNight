e08136e113439a7dcc3f27496bcbe339c6200de6
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-12-24 18:01:42 -0500

    added images and texts to shield parts

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import '../styles/App.scss';
-import { Button, Card, CardBody, CardHeader, Col, Collapse, Container, Navbar, NavbarBrand, Row } from 'reactstrap';
+import { Button, Card, CardHeader, Col, Collapse, Container, Navbar, NavbarBrand, Row } from 'reactstrap';
 
-import ShieldPart1 from '../resources/shield/shield_part_1.png';
-import ShieldPart2 from '../resources/shield/shield_part_2.png';
-import ShieldPart3 from '../resources/shield/shield_part_3.png';
+import Shield1Location1 from '../resources/shield/shield_part_1_location_1.png';
+import Shield1Location2 from '../resources/shield/shield_part_1_location_2.png';
+import Shield1Location3 from '../resources/shield/shield_part_1_location_3.png';
 
 const SHIELD_HEADER = 'shield_header';
 const SHIELD_PART = 'shield_part';
+
+const SHIELD_PART_LOCATIONS_IMAGES = [
+	[Shield1Location1, Shield1Location2, Shield1Location3],
+	['', '', ''],
+	['', '', ''],
+];
+
+const SHIELD_PART_LOCATION_ROOMS = [
+	[
+		'East Balcony',
+		'Grand Staircase',
+		'West Balcony'
+	], [
+		'East Balcony',
+		'Grand Staircase',
+		'West Balcony'
+	], [
+		'East Balcony',
+		'Grand Staircase',
+		'West Balcony'
+	],
+];
+
+const SHIELD_PART_LOCATION_DESCRIPTIONS = [
+	[
+		'To the left of the Saug wall buy, resting on a railing',
+		'To the right of Sentinel Artifact, resting on a pillar',
+		'Resting on the back wall of the west balcony'
+	], [
+		'To the left of the Saug wall buy, resting on a railing',
+		'To the right of Sentinel Artifact, resting on a pillar',
+		'Resting on the back wall of the west balcony'
+	], [
+		'To the left of the Saug wall buy, resting on a railing',
+		'To the right of Sentinel Artifact, resting on a pillar',
+		'Resting on the back wall of the west balcony'
+	],
+];
 
 class App extends Component {
 
@@ -19,7 +57,7 @@ class App extends Component {
 	};
 
 	onClick = (e) => {
-		this.setState({[e.target.id]: !this.state[e.target.id]})
+		this.setState({ [e.target.id]: !this.state[e.target.id] })
 	};
 
 	render() {
@@ -44,25 +82,34 @@ class App extends Component {
 								</CardHeader>
 
 								<Collapse toggler={`#${SHIELD_HEADER}`} isOpen={this.state[SHIELD_HEADER]} className="collapse-section">
-									{[...Array(3).keys()].map(index => {
+									{[...Array(3).keys()].map(shieldPartIndex => {
+										const locationShieldParts = SHIELD_PART_LOCATIONS_IMAGES[shieldPartIndex];
 										return (
-											<Row key={index}>
-												<Col lg={{size: '11', offset: '1'}}>
-													<CardHeader className={`sub-header ${index === 2 && this.state[`${SHIELD_PART}_${index + 1}`] ? 'show' : ''} ${this.state[`${SHIELD_PART}_${index}`] ? 'previous-open' : ''}`}>
-														<Button color="link" id={`${SHIELD_PART}_${index + 1}`} onClick={e => this.onClick(e)}>
-															Shield Part {index + 1} Locations
+											<Row key={shieldPartIndex}>
+												<Col lg={{ size: '11', offset: '1' }}>
+													<CardHeader
+														className={`sub-header ${shieldPartIndex === 2 && this.state[`${SHIELD_PART}_${shieldPartIndex + 1}`] ? 'show' : ''} ${this.state[`${SHIELD_PART}_${shieldPartIndex}`] ? 'previous-open' : ''}`}>
+														<Button color="link" id={`${SHIELD_PART}_${shieldPartIndex + 1}`} onClick={e => this.onClick(e)}>
+															Shield Part {shieldPartIndex + 1} Locations
 														</Button>
 													</CardHeader>
 
-													<Collapse toggler={`#${SHIELD_PART}_${index + 1}`} isOpen={this.state[`${SHIELD_PART}_${index + 1}`]} className="sub-collapse-section">
-														{[...Array(3).keys()].map(index => {
+													<Collapse toggler={`#${SHIELD_PART}_${shieldPartIndex + 1}`}
+													          isOpen={this.state[`${SHIELD_PART}_${shieldPartIndex + 1}`]} className="sub-collapse-section">
+														{[...Array(3).keys()].map(shieldPartLocationIndex => {
 															return (
-																<Row key={index}>
-																	<Col lg={{size: '11', offset: '1'}}>
+																<Row key={shieldPartLocationIndex}>
+																	<Col lg={{ size: '11', offset: '1' }}>
 																		<CardHeader className="sub-sub-header">
-																			<Button color="link">
-																				Blah
-																			</Button>
+																			<Row>
+																				<Col lg="3">
+																					<p className="location-room">{SHIELD_PART_LOCATION_ROOMS[shieldPartIndex][shieldPartLocationIndex]}</p>
+																					<p className="location-description">{SHIELD_PART_LOCATION_DESCRIPTIONS[shieldPartIndex][shieldPartLocationIndex]}</p>
+																				</Col>
+																				<Col lg="9">
+																					<img src={locationShieldParts[shieldPartLocationIndex]}/>
+																				</Col>
+																			</Row>
 																		</CardHeader>
 																	</Col>
 																</Row>
